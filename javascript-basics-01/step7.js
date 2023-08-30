8437fc6bfa2387190b336f94989ed73a6fa8221c
khaled.dev <salehkhaled.dev@gmail.com>
2023-08-30 10:09:15 +0300

    step7 done

---
+++ javascript-basics-01/step7.js
@@ -0,0 +1,8 @@
+const age= prompt("enter your age: ");
+const shoe_size = prompt("enter your shoe size: ");
+
+const result = () => {
+    alert((shoe_size * 2 + 5) * 50 - age + 1766);
+}
+
+result();
