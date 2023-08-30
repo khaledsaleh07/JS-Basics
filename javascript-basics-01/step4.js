79f274c0f155f09c9733ceb7472ad6199f4e4385
khaled.dev <salehkhaled.dev@gmail.com>
2023-08-30 09:38:09 +0300

    step4 done

---
+++ javascript-basics-01/step4.js
@@ -0,0 +1,5 @@
+const fname = prompt("enter your first name: ");
+const lname = prompt("enter your last name: ");
+const city = prompt("enter your city: ");
+
+alert("name: " + fname + "\nlast name: " + lname + "\ncity: " + city);
