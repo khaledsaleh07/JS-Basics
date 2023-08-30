03c283488e32b8c9fa297fceedcae5e23c1c37cf
khaled.dev <salehkhaled.dev@gmail.com>
2023-08-30 09:57:35 +0300

    step6 done

---
+++ javascript-basics-01/step6.js
@@ -0,0 +1,4 @@
+const first_n = prompt("enter first number: ");
+const second_n = prompt("enter second number: ");
+
+alert("the remainder of " + first_n + "/" + second_n + " is " + first_n%second_n);
